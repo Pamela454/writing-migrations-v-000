651612b463b212e9ba81b573457d5d5a3f55ba5b
Pamela Frieze <pfrieze@gmail.com>
2018-06-03 03:31:56 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,9 +1,7 @@
 class GradeBirthdate < ActiveRecord::Migration[5.1]
 
   def change_column()
-    add_column :student, :grade, :integer
-    add_column :student, :birthdate, :string
-    end
+    
   end
 
 end
