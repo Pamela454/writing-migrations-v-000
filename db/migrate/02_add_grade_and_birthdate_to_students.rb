76373b6a8bb9db7f492fe4b831d1c048cfd85f50
Pamela Frieze <pfrieze@gmail.com>
2018-06-03 03:25:03 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :artists do |t|
-      t.string :name
+    add_column :artists, :favorite_food, :string
     end
   end
 
